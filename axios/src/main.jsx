32fdc9829114ce7517c817717b107d13e1fb9ba4
Júlia Garcia <127803091+juliaagarcia@users.noreply.github.com>
2024-01-31 14:11:53 -0300

    add

--- axios/src/main.jsx
+++ axios/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+import { CreateBrowserRouter, RouterProvider, Router} from "react-router-dom";
 import './index.css'
+
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:"/new",
+        element: <NewPost/>
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
